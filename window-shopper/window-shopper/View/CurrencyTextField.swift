a09c0bae6dd4ca3668c32073a94bc42cdd81429c
Jakub Majewski <jakub.majewski@outlook.com>
2017-09-29 13:26:45 +0200

    First commit with class for text field

---
+++ window-shopper/window-shopper/View/CurrencyTextField.swift
@@ -0,0 +1,33 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Jakub Majewski on 29.09.2017.
+//  Copyright © 2017 Jakub Majewski. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2505351027)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil {
+            placeholder = " "
+        }
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+    }
+
+}
